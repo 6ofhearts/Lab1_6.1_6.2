600e5e490bb05d817d4138cc3632979c9d6eca53
6ofhearts <84579240+6ofhearts@users.noreply.github.com>
2022-01-23 04:21:31 +0700

    6.2.3

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import datetime
 import math
 from math import sin
 import time
+from numpy import matrix
+from numpy import linalg
 
 # Лабораторная 1, Задание 6.1
 # вычисление времени счета
@@ -163,20 +165,20 @@ m = len(A)
 x = [0. for i in range(m)]
 Iteration = 0
 converge = False
-pogr = 0.
+eps = 0.
 while not converge:
     x_new = np.copy(x)
     for i in range(m):
         s1 = sum(A[i][j] * x_new[j] for j in range(i))
         s2 = sum(A[i][j] * x[j] for j in range(i + 1, m))
         x_new[i] = (B[i] - s1 - s2) / A[i][i]
-    pogr = sum(abs(x_new[i] - x[i]) for i in range(m))
-    converge = pogr < 1e-6
+    eps = sum(abs(x_new[i] - x[i]) for i in range(m))
+    converge = eps < 1e-6
     Iteration += 1
     x = x_new
 print('Количество итераций :', Iteration)
 print('Решение системы уравнений :', x)
-print('Погрешность :', pogr)
+print('Погрешность :', eps)
 
 print("Проверка решения через numpy:", np.linalg.solve(A, B))  # проверка решения с помощью доп. библиотеки numpy
 
@@ -213,7 +215,39 @@ print('Определитель матрицы А: ', determinant(A))
 
 # 6.2.3
 
-E = [[1, 0, 0, 0], # Единичная матрица для обратного хода
+A = [
+    [4, 3, 2, 1],
+    [3, 6, 4, 2],
+    [2, 4, 6, 3],
+    [1, 2, 3, 4]
+]
+
+E = [[1, 0, 0, 0],  # Единичная матрица для обратного хода
      [0, 1, 0, 0],
      [0, 0, 1, 0],
      [0, 0, 0, 1]]
+
+WholeMatrix = [[0 for i in range(8)]] * 4
+print(WholeMatrix)
+
+
+def invert_matrix(AM, IM):
+    for fd in range(len(AM)):
+        fdScaler = 1.0 / AM[fd][fd]
+        for j in range(len(AM)):
+            AM[fd][j] *= fdScaler
+            IM[fd][j] *= fdScaler
+        print(IM[fd])
+        for i in list(range(len(AM)))[0:fd] + list(range(len(AM)))[fd + 1:]:
+            crScaler = AM[i][fd]
+            for j in range(len(AM)):
+                AM[i][j] = AM[i][j] - crScaler * AM[fd][j]
+                IM[i][j] = IM[i][j] - crScaler * IM[fd][j]
+            print(IM[i][j])
+    return IM
+
+
+print('Обратная матрица:')
+print(invert_matrix(A, E))
+
+print(np.linalg.inv(E))
